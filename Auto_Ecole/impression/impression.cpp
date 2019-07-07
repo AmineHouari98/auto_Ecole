795d8ed0b34a9e10ce5feabcff6c15198143082c
Amine Houari <amine.houari@outlook.com>
2019-07-07 16:16:41 +0100

    printing work
    +added Liste examen XML template
    +fixed return line for long names

--- Auto_Ecole/impression/impression.cpp
+++ Auto_Ecole/impression/impression.cpp
@@ -1,7 +1,10 @@
 #include "impression.h"
+#include "mydatabase.h"
+#include "qtrpt.h"
 
 impression::impression(QWidget *parent) : QWidget(parent)
 {
+
 
 }
 //DONT TOUCH THIS CODE
@@ -11,9 +14,8 @@ impression::impression(QWidget *parent) : QWidget(parent)
 //t_autoEcole *a = new t_autoEcole;
 //int rows = cad->rowCount();
 //QtRPT *report = new QtRPT(this) ;
-//report->loadReport(":/Templates/Bodereau.xml");
+//report->loadReport(":/Templates/Liste_Examen.xml");
 //report->recordCount.append(rows);
-//int i=0;
 //connect(report, &QtRPT::setValue, [&](int recNo,
 //        QString paramName, QVariant &paramValue, const int reportPage){
 
@@ -21,12 +23,17 @@ impression::impression(QWidget *parent) : QWidget(parent)
 //    if(paramName == "AdresseAuto")       paramValue = a->getAdresse();
 //    if(paramName == "NumTelAuto")        paramValue = a->getNumTel();
 //    if(paramName == "WilayaAuto")        paramValue = "Oran";
-//    if(paramName == "num")               paramValue = QString::number(recNo+1);
-//    if(paramName == "Nom")               {cad->setCurrentRow(recNo);paramValue = cad->getNom();}
-//    if(paramName == "Prenom")            {cad->setCurrentRow(recNo);paramValue = cad->getPrenom();}
+//    if(paramName == "Num")               paramValue = QString::number(recNo+1);
+//    if(paramName == "Prenom")               {cad->setCurrentRow(recNo);paramValue = cad->getPrenom();}
+//    if(paramName == "Nom")            {
+//        cad->setCurrentRow(recNo);
+//        if(cad->getNom().length()>=8)paramValue = cad->getNom()+"\n";
+//        else paramValue = cad->getNom();
+
+//    }
 //    if(paramName == "Date_De_Naissance") {cad->setCurrentRow(recNo);paramValue = cad->getDate_De_Naissance();}
 //    if(paramName == "Observations")      {cad->setCurrentRow(recNo);paramValue = "";}
-
+//    if(paramName == "NbrCandidats")      paramValue = QString::number(rows);
 
 //});
 
